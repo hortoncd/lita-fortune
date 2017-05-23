198485f2689467e7ec3127a5467518d5def15669
Javier Alvarez <javier.alvarez@xoom.com>
2017-05-23 14:33:36 -0500

    Allow user to define path to fortune binary

--- lib/lita/handlers/fortune.rb
+++ lib/lita/handlers/fortune.rb
@@ -10,8 +10,16 @@ module Lita
       def self.default_config(handler_config)
       end
 
+      # path to the fortune binary
+      config :path, type: String, required: false
+
       def fortune(response)
-        f = `fortune`
+        if config.path
+          f = `#{config.path}`
+        else
+          f = `fortune`
+        end
+
         f = "No fortune for you!" unless f
         response.reply f
       end
