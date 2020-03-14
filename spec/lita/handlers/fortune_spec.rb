b33e044ac245e2d4594e46d12df37a3a32af361e
Chris Horton <hortoncd@gmail.com>
2020-03-14 15:37:56 -0700

    mock out tests instead of relying on installed cmd

--- spec/lita/handlers/fortune_spec.rb
+++ spec/lita/handlers/fortune_spec.rb
@@ -5,8 +5,19 @@ describe Lita::Handlers::Fortune, lita_handler: true do
 
   describe 'fortune' do
     it "replies to a 'fortune' command" do
+      allow(File).to receive(:exist?).with('/usr/bin/fortune').and_return(true)
+      allow_any_instance_of(Lita::Handlers::Fortune).to receive(:run_cmd).with('/usr/bin/fortune').and_return("A magical fortune!")
       send_command('fortune')
       expect(replies.last).to be_kind_of(String)
+      expect(replies.last).to eq("A magical fortune!")
+    end
+
+    it "replies to a 'fortune' command" do
+      allow(File).to receive(:exist?).with('/usr/bin/fortune').and_return(true)
+      allow_any_instance_of(described_class).to receive(:run_cmd).with('/usr/bin/fortune').and_return("")
+      send_command('fortune')
+      expect(replies.last).to be_kind_of(String)
+      expect(replies.last).to eq("No fortune for you!")
     end
   end
 end
